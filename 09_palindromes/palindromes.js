0d8865d1ee9f14d148a7d1676b4b5c9706c965fc
BillMcBlackjack <fabrilongo407@gmail.com>
2023-12-20 14:00:07 +0100

    exercise solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+  text = text.replace(/\s/g, '').replace(/[^\w\s]|_/g, '').toLowerCase();
+  let textReverted = text.split('').reverse().join('')
+  return text === textReverted;
 };
 
 // Do not edit below this line
